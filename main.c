d664e12201e90bc56cbada28812a7dca7adc75cb
LeonardoSoaresDeSouzaFIlho <165204470+LeonardoSoaresDeSouzaFIlho@users.noreply.github.com>
2024-10-30 12:49:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#define NUM_MAX_REST 3
+
+void boasVindas (void)
+{
+	printf ("\n**************************************************");
+	printf ("\nUaiRango - Restaurante");
+	printf ("\n**************************************************");
+}
 
 int main(int argc, char const *argv[]){ 
 
-
+boasVindas;
 	
 	return 0;
 }
