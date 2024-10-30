ab324b37eb30459746a1e20886d61222a79c1fc9
LeonardoSoaresDeSouzaFIlho <165204470+LeonardoSoaresDeSouzaFIlho@users.noreply.github.com>
2024-10-30 12:39:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
-#
-#
-#
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
-{ 
+int main(int argc, char const *argv[]){ 
+
+
 	
 	return 0;
 }
